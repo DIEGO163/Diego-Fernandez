a60d49913469b71eefc35271e31217ee1e58f617
DIEGO163 <71235571+DIEGO163@users.noreply.github.com>
2021-06-22 20:38:44 -0500

    martes22

---
+++ martes22/Parroquia.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+namespace diegofernandez
+{
+    class Parroquia
+    {
+        public int ParroquiaId
+        {
+            get; set;
+        }
+        public string Nombre
+        {
+            get; set;
+        }
+        public int Habitantes
+        {
+            get; set;
+        }
+    }
+
+
+}
